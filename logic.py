e3e2a8f53525205e181f15cd13fd35b294c38130
jai-rus <jairus.legion@gmail.com>
2024-02-29 20:39:15 -0800

    first commit

---
+++ logic.py
@@ -0,0 +1,77 @@
+import random
+
+class Logic:    
+    userInGame = False
+
+    def newGame():
+        Logic.userInGame = True
+
+    def inGame():
+        return Logic.userInGame
+    
+    def endGame():
+        Logic.userInGame = False
+        return Logic.userInGame
+
+    def setPositions(word_bank):
+        pos = dict.fromkeys(range(16)) #Positions of the words
+        used = set() #Used to see if a random number has already been used
+
+        found = True
+        finish = True
+        #Creates the gameboard 
+        for key, value in pos.items():
+            found = True
+            while found and finish: #loops until the key is assigned a word
+                if value is None:
+                    r = random.randint(0, 15)
+                if r not in used:
+                    pos[key] = word_bank[r]
+                    used.add(r)
+                    found = False
+                    if (key == 16): #once all words are given a key, end
+                        finish = False
+
+        #gameBoard = ('``` ' + pos[0] + ' ' + pos[1] + ' ' + pos[2] + '' + pos[3] + '\n ' + pos[4] + ' ' + pos[5] + ' ' + pos[6] + ' ' + pos[7] + '\n '  + pos[8] + ' ' + pos[9] + ' ' + pos[10] + ' ' + pos[11] + '\n ' + pos[12] + ' ' + pos[13] + ' ' + pos[14] + ' ' + pos[15] + '```')
+
+        top = ('``` ' + pos[0] + ' ' + pos[1] + ' ' + pos[2] + ' ' + pos[3] + '```')
+        mid1 = ('``` ' + pos[4] + ' ' + pos[5] + ' ' + pos[6] + ' ' + pos[7] + '```')
+        mid2 = ('``` ' + pos[8] + ' ' + pos[9] + ' ' + pos[10] + ' ' + pos[11] + '```')
+        bot = ('``` ' + pos[12] + ' ' + pos[13] + ' ' + pos[14] + ' ' + pos[15] + '```')
+
+        return top, mid1, mid2, bot
+    
+    def checkAnswer(userInput, word_bank): 
+        
+        #splits the answer into multiple arrays
+        userInput = userInput.split()
+        
+        #categories
+        cat1 = word_bank[0:4]
+        cat2 = word_bank[4:8]
+        cat3 = word_bank[8:12]
+        cat4 = word_bank[12:16]
+
+        try:
+
+            if (set(userInput) == set(cat1)):
+                return True, 0 #returns True and the position for completed words
+            elif (set(userInput) == set(cat2)):
+                return True, 1
+            elif (set(userInput) == set(cat3)):
+                return True, 2
+            elif (set(userInput) == (cat4)):
+                return True, 3
+            else:
+                return False, -1
+
+        except:
+            return False, -1
+        
+   # def newBoard(answer):
+    #make new gameboard with smaller array
+
+
+                
+                
+
---
+++ main.py
@@ -0,0 +1,75 @@
+import discord
+from discord.ext import commands
+import logic
+import asyncio
+import config
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+#checks whether user is in game
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    print('---------')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if message.content.startswith('!play'):
+        global word_bank
+        word_bank = ("basic", "java", "python", "ruby", "croc", "loafer", "moccasin", "slipper", "boa", "headdress", "pillow", "shuttlecock", "cobra", "inspiration", "lightning", "union")
+
+        gameBoard = logic.Logic.setPositions(word_bank)
+
+        await message.channel.send(gameBoard[0])
+        await message.channel.send(gameBoard[1])
+        await message.channel.send(gameBoard[2])
+        await message.channel.send(gameBoard[3])
+
+        logic.Logic.newGame()
+
+    if message.content.startswith('!answer'):
+        completedWords = [0, 0, 0, 0] #Used to see if they already used an answer set
+        tries = 4 #Amount of tries the user gets
+
+        while (logic.Logic.inGame()): 
+            try:
+                guess = await bot.wait_for('message', timeout=500.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send("You took too long")
+
+            check = logic.Logic.checkAnswer(guess.content, word_bank)
+
+            #checks if its a correct answer
+            if (check[0]): 
+                if (completedWords[check[1]] == 0): #checks if it already exists in completedWords, if not set its position equal to 1
+                    await message.channel.send("Correct!")
+                    await message.channel.send("You have " + str(tries) + " tries left") 
+                    completedWords[check[1]] = 1 #TODO: Make it reprint the gameboard depending on how many words are left
+                else:
+                    tries -= 1
+                    await message.channel.send("You've already done this one")
+                    await message.channel.send("You have " + str(tries) + " tries left") 
+                    
+            else:
+              tries -= 1
+              await message.channel.send("Wrong!")
+              await message.channel.send("You have " + str(tries) + " tries left") 
+
+            if (tries == 0):
+                await message.channel.send("You lost! Loser stinky")
+                logic.Logic.endGame()
+                break
+
+        if (not logic.Logic.inGame()):
+            await message.channel.send("Use !play to start a game")
+            
+
+
+bot.run(config.token)
